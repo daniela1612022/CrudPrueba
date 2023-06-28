a40f7c4c2307883b401f71cca15cfbaa153980df
daniela.16 <64100428+daniela1612022@users.noreply.github.com>
2023-06-28 12:19:51 -0500

    Update enviroment.ts

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,12 +1,12 @@
 export const enviroment = {
   firebase : {
 
-      apiKey: "AIzaSyD46TEm1dtbDAdjQAqI-QwBpXqh1bit-j4",
-      authDomain: "angular-firebase-8617d.firebaseapp.com",
-      projectId: "angular-firebase-8617d",
-      storageBucket: "angular-firebase-8617d.appspot.com",
-      messagingSenderId: "997036680571",
-      appId: "1:997036680571:web:513a2f15210a8d8a8149de"
+      apiKey: *,
+      authDomain: *,
+      projectId: *,
+      storageBucket: *,
+      messagingSenderId: *,
+      appId: *
 
   },
 }
